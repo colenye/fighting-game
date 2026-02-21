4cf46da4c357d2b4608d7ab634dee8078866a885
Cole <colenye529@gmail.com>
2026-02-21 18:17:05 -0500

    added jab and cross, also enemies face eachother now (there's support for more than 2 players but players only lock onto 1 player

--- Characters/character_body_2d.gd
+++ Characters/character_body_2d.gd
@@ -2,39 +2,53 @@ class_name Fighter
 extends CharacterBody2D
 
 static var scene = preload("res://Characters/Fighter.tscn")
-const SPEED := 50
-const JUMP_VELOCITY = -400.0
+const SPEED := 300
+const JUMP_VELOCITY = -600.0
 const JABDMG := 5
 const CROSSDMG := 10
 var player : int
-var pStr : String
+
 var otherPlayer : int
 var health := 100
 @export var hurtbox : Area2D
 @export var anim : AnimationPlayer
+@export var pStr : String
+@export var otherPlayerCharacter : CharacterBody2D
 
-static func init(position := [50,50], player := 1):
+static func init(position: Vector2, player_id: int, input_prefix: String):
 	var inst = scene.instantiate()
-	inst.global_position.x = position[0]
-	inst.global_position.y = position[1]
-	inst.player = player
-	inst.otherPlayer = 3 - player
-	inst.pStr = "p" + str(player) + "_"
-	inst.hurtbox.playerID = player
+	inst.global_position = position
+	inst.player = player_id
+	inst.pStr = input_prefix
+	inst.hurtbox.playerID = player_id
 	return inst
 
+func set_other_player(other_player : CharacterBody2D) -> void:
+	otherPlayerCharacter = other_player
+
+# 
+func face_opponent():
+	if otherPlayerCharacter:
+		if otherPlayerCharacter.global_position.x > global_position.x:
+			
+			$Sprite2D.scale.x = 1
+		else:
+			$Sprite2D.scale.x = -1
+			
+			
+			
 func attack():
 	if Input.is_action_just_pressed(pStr + "jab"):
 		anim.play("jab")
 	if Input.is_action_just_pressed(pStr + "cross"):
-		anim.play(pStr + "cross")
+		anim.play("cross")
 func movement(delta: float):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed(pStr + "jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -55,9 +69,18 @@ func _physics_process(delta: float) -> void:
 	
 	movement(delta)
 	attack()
+	face_opponent()
 	move_and_slide()
 
 # For the cross
-func _on_area_2d_area_entered(area: Area2D) -> void:
+
+
+
+func _on_area_2d_area_entered_jab(area: Area2D) -> void:
+	if area is HurtBox and area.playerID != player:
+		area.take_damage(JABDMG)
+
+
+func _on_area_2d_area_entered_cross(area: Area2D) -> void:
 	if area is HurtBox and area.playerID != player:
 		area.take_damage(CROSSDMG)
--- Levels/level.gd
+++ Levels/level.gd
@@ -1,13 +1,16 @@
+class_name Level
 extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var p1 = Fighter.init([50,50], 1)
-	var p2 = Fighter.init([500,50], 2)
+	var p1 = Fighter.init(Vector2(50,50), 1, "p1_")
+	var p2 = Fighter.init(Vector2(500,50), 2, "p2_")
+	p1.set_other_player(p2)
+	p2.set_other_player(p1)
 	add_child(p1)
 	add_child(p2)
-	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
